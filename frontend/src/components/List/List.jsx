7bc81ebe715795f769d56783d73e6905c8f15f26
StasDemidovich <demidovichstas98@gmail.com>
2023-07-03 14:07:17 +0300

    add pagination, change sorting slider by price

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -3,16 +3,38 @@ import Card from '../Card/Card'
 import useFetch from './../../hooks/useFetch'
 import './List.scss'
 
-const List = ({ subCats, maxPrice, sort, catId }) => {
+const List = ({ subCats, sort, catId, page, minPrice, maxPrice }) => {
+  
+  const qs = require('qs');
+  const query = qs.stringify({
+    populate: '*',
+    filters: {
+      categories: {
+        id: `${catId}`,
+      },
+      sub_categories: {
+        id: {
+          $eq: subCats
+        },
+      },
+      price: {
+        $gt: `${minPrice}`,
+        $lte: `${maxPrice}`
+      },
+    },
+    sort: `price:${sort}`,
+    pagination: {
+      page: `${page}`,
+      pageSize: 6
+    } 
+  }, 
+  {
+  encodeValuesOnly: true, // prettify URL
+});
 
-  const filter = subCats.map(
-    (item) => `&[filters][sub_categories][id][$eq]=${item}`
-).join('')
-
-  const { data, loading } = useFetch(
-    `/products?populate=*&[filters][categories][id]=${catId}${filter}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
-  );
-
+const { data, loading } = useFetch(
+  `/products?${query}`
+);
 
   return (
     <div className="list">
--- frontend/src/pages/Products/Products.jsx
+++ frontend/src/pages/Products/Products.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, {useState } from 'react'
 import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import List from './../../components/List/List';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import Slider from 'react-slider'
 import './Products.scss'
 
 const Products = () => {
 
+  const MIN = 0;
+  const MAX = 200;
+
   const catId = parseInt(useParams().id)
-  const [maxPrice, setMaxPrice] = useState(200);
   const [sort, setSort] = useState('asc');
   const [selectedSubCats, setSelectedSubCats] = useState([]);
+  const [page, setPage] = useState(1);
+  const [values, setValues] = useState([MIN, MAX])
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
 
   const { data } = useFetch(
     `/sub-categories?[filters][categories][id][$eq]=${catId}`
@@ -29,21 +41,33 @@ const Products = () => {
           <h2>Product Categories</h2>
           {data?.map((item) => (
             <div className="inputItem" key={item.id}>
-              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange}/>
-              <label htmlFor={item.id}>{item.attributes.title}</label>
+              <div className='input'>
+                <input type="checkbox" id={item.id} value={item.id} onChange={handleChange}/>
+                <label htmlFor={item.id}>{item.attributes.title}</label>
+              </div>
             </div>
           ))}
         </div>
         <div className="filterItem">
-          <h2>Filter by price</h2>
-          <div className='inputItem'>
-            <span>0</span>
-            <input type="range" min={0} max={200} onChange={(e) => setMaxPrice(e.target.value)}/>
-            <span>{maxPrice}</span>
+          <div className='filters'>
+            <div className='filters-price'>
+              <h3>Sort by price</h3>
+              <div className='values'>${values[0]} - ${values[1]}</div>
+              <small>
+                Current range: ${values[1] - values[0]}
+              </small>
+              <div className="filters-price__slider" id='slider'>
+                <Slider 
+                  value={values} 
+                  className='filters-slider' 
+                  min={MIN} 
+                  max={MAX} 
+                  onChange={setValues}/>
+              </div>
+            </div>
           </div>
         </div>
         <div className="filterItem">
-          <h2>Sort by</h2>
           <div className="inputItem">
             <input type="radio" id='asc' value="asc" name='price' onChange={(e) => setSort("asc")}/>
             <label htmlFor="asc">Price (Lowest first)</label>
@@ -66,8 +90,14 @@ const Products = () => {
           src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" 
           alt="" 
         />
-      }
-      <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats}/>
+        }
+        <List page={page} catId={catId} minPrice={values[0]} maxPrice={values[1]} sort={sort} subCats={selectedSubCats}/>
+        <div className="pages">
+          <Stack spacing={2}>
+            <Typography className='pagination'>Page: {page}</Typography>
+            <Pagination count={2} page={page} onChange={handlePageChange} />
+          </Stack>
+        </div>
       </div>
     </div>
   )
